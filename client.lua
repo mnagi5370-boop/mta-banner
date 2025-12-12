5578bcdf624ede6bab1132d140782a1d655dd83f
mnagi5370-boop <mnagi5370@gmail.com>
2025-12-12 03:01:50 +0200

    Implement fullscreen banner using dxDrawImage
    Add fullscreen banner display on client render.

--- client.lua
+++ client.lua
@@ -1,1 +1,10 @@
+local sx, sy = guiGetScreenSize()  -- معرفة حجم شاشة اللاعب
+local bannerTexture = dxCreateTexture("images/banner.png")  -- يقرأ الصورة
 
+addEventHandler("onClientRender", root,
+    function()
+        if bannerTexture then
+            dxDrawImage(0, 0, sx, sy, bannerTexture)  -- يعرض الصورة Full Screen
+        end
+    end
+)
